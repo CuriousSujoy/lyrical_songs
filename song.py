8f27c9792500d91ff7023c847deeb34b2b0bda52
Sujoy Gorai <sujoygorai651@gmail.com>
2026-06-08 14:25:12 +0530

    Safar by Bayaan and Sherazam

--- song.py
+++ song.py
@@ -2,69 +2,78 @@ import time
 import sys
 import pygame
 
-# Typing animation for each character
+# Character by Character printing
 def type_lyric(text):
     for char in text:
         sys.stdout.write(char)
         sys.stdout.flush()
         time.sleep(0.03)
     print()
-
+#List of the lyrics (As multiple string so that I can adjust the delay between each sentence printing)
 lyrics = [
-    Dikhte hain khwaab jo jaagti aankhon ko
+    "\nDikhte hain khwaab jo jaagti aankhon ko",
 
-Sapne nahi, iraade hain
+    "\nSapne nahi, iraade hain",
 
-Poore woh karne ko chal pade nange pairon
+    "\nPoore woh karne ko chal pade nange pairon",
 
-Khud se kiye jo waade hain
+    "\nKhud se kiye jo waade hain",
 
-Rukna naa kabhi bhi tha hal
+    "\nRukna naa kabhi bhi tha hal",
 
-"\nChalna hi to hai manzil"
+    "\nChalna hi to hai manzil",
 
-"\nMain naa kaheen thehra"
+    "\nMain naa kaheen thehra",
 
-"\nMain bhaaga-bhaaga, sadiyon se jaaga"
+    "\nmusic.........",
 
-"\nAayaa hoon meelon gaaon se"
+    "\nMain bhaaga-bhaaga, sadiyon se jaaga",
 
-"\nNaa humsafar koi, naa apna ghar, yaara"
+    "\nAayaa hoon meelon gaaon se",
 
-"\nRehta hoon behti naaon pe"
+    "\nmusic.........",
 
-"\nMain bhaaga-bhaaga, sadiyon se jaaga"
+    "\nNaa humsafar koi, naa apna ghar, yaara",
 
-"\nAayaa hoon meelon kaarke iraada"
+    "\nRehta hoon behti naaon pe.....",
 
-"\nMain bhaaga-bhaaga, sadiyon se jaaga"
+    "\nMain bhaaga-bhaaga, sadiyon se jaaga",
 
-"\nAayaa hoon meelon kaarke iraada"
+    "\nAayaa hoon meelon kaarke iraada",
+
+    "\nMain bhaaga-bhaaga, sadiyon se jaaga",
+
+    "\nAayaa hoon meelon kaarke iraada"
 ]
 
-# Delays matched to song pacing (approximate, refine by listening carefully)
+# Delays matched to song pacing (approximate, refined by listening carefully)
 delays = [
-    2.0, 3, 3.5,
-    2.0, 3.5, 2.5,
-    2.5, 2.5, 2.5,
-    3.0, 2.5, 3.0
+    4.0, 4.0, 3.0, 3.2,
+    2.0, 2.0, 2.3, 1.0,
+    2.0, 4.0, 0.7, 3.1,
+    4.0, 3.1, 3.6, 4.0,
+    3.6
 ]
 
+#Below function is responsible for printing sentence by sentence animation, delays is used in this function
 def print_lyrics():
-    print("\nMain Tera – Arijit Singh (Kalank)\n")
+    print("\nSafar\n")
     time.sleep(1.5)
 
     # Initialize pygame mixer
     pygame.mixer.init()
-    pygame.mixer.music.load("D:\Song_Project\main_tera_clip (mp3cut.net).mp3")  # <-- put your audio file here
+
+    # Load audio file from project folder
+    pygame.mixer.music.load("Song_Clips/Safar_clip.mp3")
     pygame.mixer.music.play()
 
-    # Animate lyrics in sync
+    # Animating lyrics in sync
     for i, line in enumerate(lyrics):
         type_lyric(line)
         time.sleep(delays[i])
 
     print("\nEnd of Lyrics\n")
+    print("\nSubscribe to the youtube channel or instagram page\nFollow CuriousSujoy on GitHub for such programs with other songs")
 
     # Stop music after lyrics end
     pygame.mixer.music.stop()
